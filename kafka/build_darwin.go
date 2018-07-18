6fa98e5aa79d53984f27ba001c93ae380f587ab4
Bene <bene@peernova.com>
2018-07-18 11:10:16 -0700

    removing openssl sasl shared library dependency

--- kafka/build_darwin.go
+++ kafka/build_darwin.go
@@ -3,6 +3,6 @@
 package kafka
 
 // #cgo CFLAGS: -I${SRCDIR}/../../../../../3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/liblz4.a -lcrypto -lssl -lz -lm -lsasl2 -ldl
+// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/liblz4.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libsasl2.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libcrypto.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libssl.a -lz -lm -ldl
 //
 import "C"
