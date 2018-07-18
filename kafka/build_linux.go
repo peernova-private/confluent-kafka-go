82f0d644ae79a733e7b109e4d4ac3554d00f2884
Bene <bene@peernova.com>
2018-07-18 12:46:01 -0700

    fixed linking order

--- kafka/build_linux.go
+++ kafka/build_linux.go
@@ -3,6 +3,6 @@
 package kafka
 
 // #cgo CFLAGS: -I${SRCDIR}/../../../../../3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/liblz4.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libsasl2.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libcrypto.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libssl.a -lz -lm -ldl
+// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/liblz4.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libsasl2.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libssl.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libcrypto.a -lz -lm -ldl
 //
 import "C"
