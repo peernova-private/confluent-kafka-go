e209c698f2df80f15231c20ee00337248cb5cdfc
Christopher Krowl <chrisk@peernova.com>
2019-05-24 14:32:34 -0700

    Remove relative path

--- kafka/build_darwin.go
+++ kafka/build_darwin.go
@@ -2,7 +2,6 @@
 
 package kafka
 
-// #cgo CFLAGS: -I${SRCDIR}/../../../../../3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/liblz4.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libsasl2.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libcrypto.a ${SRCDIR}/../../../../../3rdparty/lib/darwin_amd64/libssl.a -lz -lm -ldl
+// #cgo LDFLAGS: -lrdkafka -llz4 -lsasl2 -lssl -lcrypto -lz -lm -ldl -lrt
 //
 import "C"
--- kafka/build_linux.go
+++ kafka/build_linux.go
@@ -2,7 +2,6 @@
 
 package kafka
 
-// #cgo CFLAGS: -I${SRCDIR}/../../../../../3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/librdkafka.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/liblz4.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libsasl2.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libssl.a ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/libcrypto.a -lz -lm -ldl -lrt
+// #cgo LDFLAGS: -lrdkafka -llz4 -lsasl2 -lssl -lcrypto -lz -lm -ldl -lrt
 //
 import "C"
