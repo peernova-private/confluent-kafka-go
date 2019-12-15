f3770f0f1d773d8bcaba3e0ab8de683fa1b8ca65
bene <bene@peernova.com>
2019-12-14 21:35:34 -0800

    fixed paths

--- kafka/build_darwin.go
+++ kafka/build_darwin.go
@@ -2,7 +2,7 @@
 
 package kafka
 
-// #cgo CFLAGS: -I${SRCDIR}/3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/3rdparty/lib/darwin_amd64/liblz4.a ${SRCDIR}/3rdparty/lib/darwin_amd64/libsasl2.a ${SRCDIR}/3rdparty/lib/darwin_amd64/libcrypto.a ${SRCDIR}/3rdparty/lib/darwin_amd64/libssl.a -lz -lm -ldl
+// #cgo CFLAGS: -I${SRCDIR}/../3rdparty/include
+// #cgo LDFLAGS: ${SRCDIR}/../3rdparty/lib/darwin_amd64/librdkafka.a ${SRCDIR}/../3rdparty/lib/darwin_amd64/liblz4.a ${SRCDIR}/../3rdparty/lib/darwin_amd64/libsasl2.a ${SRCDIR}/../3rdparty/lib/darwin_amd64/libcrypto.a ${SRCDIR}/../3rdparty/lib/darwin_amd64/libssl.a -lz -lm -ldl
 //
 import "C"
